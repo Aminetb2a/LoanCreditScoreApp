9a16749659cb01db826f77d73cc849fa6872c125
aminet2a <aminetbo@gmail.com>
2023-02-21 20:42:16 +0100

    add User entity

---
+++ src/main/java/patika/dev/definex/loanCreditScore/entity/User.java
@@ -0,0 +1,26 @@
+package patika.dev.definex.loanCreditScore.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.annotation.processing.Generated;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("user")
+public class User {
+    @Id
+    private String id;
+    private Long idNo;
+    private String name;
+    private Double income;
+    private String surname;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date birthDate;
+    private String phoneNumber;
+    private Long collateralIdNo;
+}
