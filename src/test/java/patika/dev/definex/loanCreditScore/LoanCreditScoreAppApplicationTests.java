3e2f9b367a4bfd684a310db65368450075ba82a9
aminet2a <aminetbo@gmail.com>
2023-02-21 08:42:28 +0100

    initial commit

---
+++ src/test/java/patika/dev/definex/loanCreditScore/LoanCreditScoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package patika.dev.definex.loanCreditScore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCreditScoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
