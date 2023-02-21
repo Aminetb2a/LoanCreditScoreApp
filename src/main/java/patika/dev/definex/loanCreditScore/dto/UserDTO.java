a678aeec79856ac750af1910d477f78223420906
aminet2a <aminetbo@gmail.com>
2023-02-21 21:35:34 +0100

    add UserDTOMapper

--- src/main/java/patika/dev/definex/loanCreditScore/dto/UserDTO.java
+++ src/main/java/patika/dev/definex/loanCreditScore/dto/UserDTO.java
@@ -1,15 +1,18 @@
 package patika.dev.definex.loanCreditScore.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Accessors(chain = true)
 public class UserDTO {
     private String id;
     private Long idNo;
---
+++ src/main/java/patika/dev/definex/loanCreditScore/dto/mapper/UserDTOMapper.java
@@ -0,0 +1,28 @@
+package patika.dev.definex.loanCreditScore.dto.mapper;
+
+import patika.dev.definex.loanCreditScore.controller.request.UserRequest;
+import patika.dev.definex.loanCreditScore.dto.UserDTO;
+import patika.dev.definex.loanCreditScore.model.User;
+
+public class UserDTOMapper {
+    public UserDTO mapToModel(UserRequest userRequest) {
+        return new UserDTO()
+                .setIdNo(userRequest.getIdNo())
+                .setName(userRequest.getName())
+                .setIncome(userRequest.getIncome())
+                .setSurname(userRequest.getSurname())
+                .setBirthDate(userRequest.getBirthDate())
+                .setPhoneNumber(userRequest.getPhoneNumber());
+    }
+
+    public UserDTO mapToModel(User user) {
+        return new UserDTO()
+                .setId(user.getId())
+                .setIdNo(user.getIdNo())
+                .setName(user.getName())
+                .setIncome(user.getIncome())
+                .setSurname(user.getSurname())
+                .setBirthDate(user.getBirthDate())
+                .setPhoneNumber(user.getPhoneNumber());
+    }
+}
