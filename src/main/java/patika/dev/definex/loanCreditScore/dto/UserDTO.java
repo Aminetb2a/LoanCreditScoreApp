51b0366029e5fd8d3a83718b62cfa04bcc4c02c0
aminet2a <aminetbo@gmail.com>
2023-02-22 08:28:43 +0100

    update userDTO

--- src/main/java/patika/dev/definex/loanCreditScore/dto/UserDTO.java
+++ src/main/java/patika/dev/definex/loanCreditScore/dto/UserDTO.java
@@ -1,7 +1,8 @@
 package patika.dev.definex.loanCreditScore.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,12 +14,14 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 @Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDTO {
     private String id;
     private Long idNo;
     private String name;
     private Double income;
     private String surname;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date birthDate;
     private String phoneNumber;
     private Long collateralIdNo;
