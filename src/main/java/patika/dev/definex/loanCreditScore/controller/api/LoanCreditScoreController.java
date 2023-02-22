4344d4c2550aae510b99717cfaa1215bdf48cbf1
aminet2a <aminetbo@gmail.com>
2023-02-22 23:04:09 +0100

    update createUser service

--- src/main/java/patika/dev/definex/loanCreditScore/controller/api/LoanCreditScoreController.java
+++ src/main/java/patika/dev/definex/loanCreditScore/controller/api/LoanCreditScoreController.java
@@ -1,14 +1,19 @@
 package patika.dev.definex.loanCreditScore.controller.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import patika.dev.definex.loanCreditScore.controller.request.UserRequest;
 import patika.dev.definex.loanCreditScore.dto.mapper.UserDTOMapper;
+import patika.dev.definex.loanCreditScore.model.BaseResponse;
 import patika.dev.definex.loanCreditScore.service.UserService;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class LoanCreditScoreController {
@@ -16,6 +21,10 @@ public class LoanCreditScoreController {
 
     @PostMapping("user/add")
     public ResponseEntity addUser(@RequestBody UserRequest userRequest){
-        return ResponseEntity.ok( userService.createUser(new UserDTOMapper().mapToModel(userRequest)));
+        BaseResponse response = userService.createUser(new UserDTOMapper().mapToModel(userRequest));
+        return ResponseEntity.status(response.getSuccess()
+                ? HttpStatus.OK.value()
+                :HttpStatus.BAD_REQUEST.value())
+                .body(response);
     }
 }
--- src/main/java/patika/dev/definex/loanCreditScore/service/UserService.java
+++ src/main/java/patika/dev/definex/loanCreditScore/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import patika.dev.definex.loanCreditScore.dto.UserDTO;
 import patika.dev.definex.loanCreditScore.dto.mapper.UserDTOMapper;
+import patika.dev.definex.loanCreditScore.model.BaseResponse;
 import patika.dev.definex.loanCreditScore.model.User;
 import patika.dev.definex.loanCreditScore.model.mapper.UserMapper;
 import patika.dev.definex.loanCreditScore.repository.UserRepository;
@@ -14,10 +15,30 @@ import patika.dev.definex.loanCreditScore.repository.UserRepository;
 public class UserService {
     private static final UserMapper mapper = new UserMapper();
     private static final UserDTOMapper mapperDTO = new UserDTOMapper();
+    private static final BaseResponse result = new BaseResponse();
     private final UserRepository userRepository;
 
-    public UserDTO createUser(UserDTO userDTO) {
-        User response = userRepository.save(mapper.mapToUser(userDTO));
-        return mapperDTO.mapToModel(response);
+    public BaseResponse createUser(UserDTO userDTO) {
+        User user = mapper.mapToUser(userDTO);
+        try {
+            User response = userRepository.save(user);
+            return getSuccess(mapperDTO.mapToModel(response));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return getFailure(ex.getMessage());
+        }
+    }
+
+
+    private BaseResponse getSuccess(Object data){
+        result.setSuccess(true);
+        result.setData(data);
+        return result;
+    }
+
+    private BaseResponse getFailure(String message){
+        result.setSuccess(false);
+        result.setMessage(message);
+        return result;
     }
 }
