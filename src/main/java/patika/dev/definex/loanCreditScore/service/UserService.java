ce21e000785d4ab0e966dfd59a68737d5177071f
aminet2a <aminetbo@gmail.com>
2023-02-22 08:31:28 +0100

    add createUser service

--- src/main/java/patika/dev/definex/loanCreditScore/service/UserService.java
+++ src/main/java/patika/dev/definex/loanCreditScore/service/UserService.java
@@ -3,9 +3,21 @@ package patika.dev.definex.loanCreditScore.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import patika.dev.definex.loanCreditScore.dto.UserDTO;
+import patika.dev.definex.loanCreditScore.dto.mapper.UserDTOMapper;
+import patika.dev.definex.loanCreditScore.model.User;
+import patika.dev.definex.loanCreditScore.model.mapper.UserMapper;
+import patika.dev.definex.loanCreditScore.repository.UserRepository;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private UserService userService;
+    private static final UserMapper mapper = new UserMapper();
+    private static final UserDTOMapper mapperDTO = new UserDTOMapper();
+    private final UserRepository userRepository;
 
+    public UserDTO createUser(UserDTO userDTO) {
+        User response = userRepository.save(mapper.mapToUser(userDTO));
+        return mapperDTO.mapToModel(response);
+    }
 }
