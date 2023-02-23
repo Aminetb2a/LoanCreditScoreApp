d7b3603c47e31935051850bb95b3a574d96d7573
aminet2a <aminetbo@gmail.com>
2023-02-23 22:31:12 +0100

    update GlobalExceptionHandler

--- src/main/java/patika/dev/definex/loanCreditScore/config/advice/GlobalExceptionHandler.java
+++ src/main/java/patika/dev/definex/loanCreditScore/config/advice/GlobalExceptionHandler.java
@@ -2,7 +2,11 @@ package patika.dev.definex.loanCreditScore.config.advice;
 
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import patika.dev.definex.loanCreditScore.model.BaseResponse;
 
 @RestControllerAdvice
@@ -10,19 +14,23 @@ public class GlobalExceptionHandler extends RuntimeException {
 
     @ExceptionHandler({DuplicateKeyException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public @ResponseBody BaseResponse duplicateKeyExceptionHandler(DuplicateKeyException ex){
-        return BaseResponse.builder()
-                .message(ex.getCause().getMessage())
-                .success(false)
-                .build();
+    public @ResponseBody ResponseEntity<Object> duplicateKeyExceptionHandler(DuplicateKeyException ex) {
+        return getResponse(ex.getCause().getMessage());
     }
 
     @ExceptionHandler({IllegalArgumentException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public @ResponseBody BaseResponse illegalStateExceptionHandler(IllegalArgumentException ex){
-        return BaseResponse.builder()
-                .message(ex.getMessage())
-                .success(false)
-                .build();
+    public @ResponseBody ResponseEntity<Object> illegalStateExceptionHandler(IllegalArgumentException ex) {
+        return getResponse(ex.getMessage());
+    }
+
+    private ResponseEntity<Object> getResponse(String message) {
+        return ResponseEntity
+                .badRequest()
+                .body(BaseResponse.builder()
+                        .message(message)
+                        .success(false)
+                        .build()
+                );
     }
 }
