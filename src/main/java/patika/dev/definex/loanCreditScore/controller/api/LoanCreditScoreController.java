ca74522822030011e848721181d50e9633cf6b43
aminet2a <aminetbo@gmail.com>
2023-02-23 22:29:03 +0100

    add validation service

--- src/main/java/patika/dev/definex/loanCreditScore/controller/api/LoanCreditScoreController.java
+++ src/main/java/patika/dev/definex/loanCreditScore/controller/api/LoanCreditScoreController.java
@@ -1,8 +1,6 @@
 package patika.dev.definex.loanCreditScore.controller.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +11,9 @@ import patika.dev.definex.loanCreditScore.dto.UserDTO;
 import patika.dev.definex.loanCreditScore.dto.mapper.UserDTOMapper;
 import patika.dev.definex.loanCreditScore.service.impl.UserServiceImpl;
 
+import javax.validation.Valid;
+
+
 @Validated
 @RestController
 @RequiredArgsConstructor
@@ -20,12 +21,12 @@ public class LoanCreditScoreController {
     private final UserServiceImpl userService;
 
     @PostMapping("user/add")
-    public UserDTO addUser(@RequestBody UserRequest userRequest){
+    public UserDTO addUser(@Valid @RequestBody UserRequest userRequest) {
         return userService.createUser(new UserDTOMapper().mapToModel(userRequest));
     }
 
     @PutMapping("user/update")
-    public UserDTO updateUser(@RequestBody UserRequest userRequest){
+    public UserDTO updateUser(@Valid @RequestBody UserRequest userRequest) {
         return userService.updateUser(new UserDTOMapper().mapToModel(userRequest));
     }
 }
--- src/main/java/patika/dev/definex/loanCreditScore/controller/request/UserRequest.java
+++ src/main/java/patika/dev/definex/loanCreditScore/controller/request/UserRequest.java
@@ -5,20 +5,32 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserRequest {
-    // TODO add validation
     private String id;
+    @Positive
+    @Digits(integer = 15, fraction = 0)
     private Long idNo;
+    @NotBlank
+    @Size(min = 2, max = 20)
     private String name;
+    @Positive
     private Double income;
+    @NotBlank
+    @Size(min = 2, max = 20)
     private String surname;
+    @Past
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date birthDate;
+    @NotBlank
+    @Pattern(regexp = "^\\+\\d{12}$", message = "The phone number must have 12 characters of format +xxxyyyy...")
     private String phoneNumber;
+    @Digits(integer = 15, fraction = 0)
+    @Positive
     private Long collateralIdNo;
 }
