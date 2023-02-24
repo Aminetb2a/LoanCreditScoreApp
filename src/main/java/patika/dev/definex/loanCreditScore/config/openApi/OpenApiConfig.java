6c6cb03cbbb5de2c14e3203852ac88e27ad3a4dd
aminet2a <aminetbo@gmail.com>
2023-02-24 14:40:59 +0100

    add openApi configurations and annotations in controller

---
+++ src/main/java/patika/dev/definex/loanCreditScore/config/openApi/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package patika.dev.definex.loanCreditScore.config.openApi;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Loan Credit Score API API", version = "0.1",
+        contact = @Contact(name = "aminetb2a", email = "aminetbo@gmail.com"))
+)
+public class OpenApiConfig {
+
+//    @Bean
+//    public OpenAPI springOpenAPI() {
+//        return new OpenAPI().info(new Info() //
+//                .title("Loan Credit Score API") //
+//                .description("Loan Credit Score API Simple Application") //
+//                .version("0.0.1"));
+//    }
+
+}
--- src/main/java/patika/dev/definex/loanCreditScore/controller/api/LoanCreditScoreController.java
+++ src/main/java/patika/dev/definex/loanCreditScore/controller/api/LoanCreditScoreController.java
@@ -1,30 +1,52 @@
 package patika.dev.definex.loanCreditScore.controller.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import patika.dev.definex.loanCreditScore.controller.request.UserRequest;
 import patika.dev.definex.loanCreditScore.dto.UserDTO;
 import patika.dev.definex.loanCreditScore.dto.mapper.UserDTOMapper;
 import patika.dev.definex.loanCreditScore.service.impl.UserServiceImpl;
 
-import javax.validation.Valid;
-
 
 @Validated
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/loan")
 public class LoanCreditScoreController {
     private final UserServiceImpl userService;
 
+    @Operation(summary = "Create a User")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content)})
     @PostMapping("user/add")
+    @ResponseStatus(HttpStatus.CREATED)
     public UserDTO addUser(@Valid @RequestBody UserRequest userRequest) {
         return userService.createUser(new UserDTOMapper().mapToModel(userRequest));
     }
 
+    @Operation(summary = "Update User")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User updated successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content)})
     @PutMapping("user/update")
     public UserDTO updateUser(@Valid @RequestBody UserRequest userRequest) {
         return userService.updateUser(new UserDTOMapper().mapToModel(userRequest));
