e61736b4d63907d41d867adf53acb0acd9491e24
aminet2a <aminetbo@gmail.com>
2023-02-25 00:13:11 +0100

    add findByIdNoAndBirthDate and findByIdNo to UserRepository

--- src/main/java/patika/dev/definex/loancreditscore/repository/UserRepository.java
+++ src/main/java/patika/dev/definex/loancreditscore/repository/UserRepository.java
@@ -3,5 +3,12 @@ package patika.dev.definex.loancreditscore.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import patika.dev.definex.loancreditscore.model.user.User;
 
+import java.util.Date;
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    //    @Query(value = "{'idNo' : ?0,'birthDate' : ?1}")
+    Optional<User> findByIdNoAndBirthDate(Long idNo, Date birthDate);
+
+    Optional<User> findByIdNo(Long idNo);
 }
