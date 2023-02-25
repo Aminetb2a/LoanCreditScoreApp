519d88608f2b73de28634350eba8496f99b14ff0
aminet2a <aminetbo@gmail.com>
2023-02-25 19:26:50 +0100

    update Income ranges

--- src/main/java/patika/dev/definex/loancreditscore/constant/IncomeRange.java
+++ src/main/java/patika/dev/definex/loancreditscore/constant/IncomeRange.java
@@ -3,7 +3,7 @@ package patika.dev.definex.loancreditscore.constant;
 import org.apache.commons.lang3.Range;
 
 public interface IncomeRange {
-    Range<Double> LOW = Range.between(0.0, 5000.0);
-    Range<Double> MEDIUM = Range.between(5000.0, 10000.0);
+    Range<Double> LOW = Range.between(0.0, 4999.999d);
+    Range<Double> MEDIUM = Range.between(5000.0, 9999.999);
     Range<Double> HIGH = Range.between(10000.0, Double.MAX_VALUE);
 }
