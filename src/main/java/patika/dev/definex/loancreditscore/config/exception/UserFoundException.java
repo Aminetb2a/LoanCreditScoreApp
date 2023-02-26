7e3ece1f82a45e15f0599381ddaf53eccfe23665
aminet2a <aminetbo@gmail.com>
2023-02-26 18:12:39 +0100

    add comments explaining the purpose of the exception classes added

--- src/main/java/patika/dev/definex/loancreditscore/config/exception/UserFoundException.java
+++ src/main/java/patika/dev/definex/loancreditscore/config/exception/UserFoundException.java
@@ -1,5 +1,8 @@
 package patika.dev.definex.loancreditscore.config.exception;
 
+/**
+ * UserFoundException is a RuntimeException that is thrown when a user exists already.
+ */
 public class UserFoundException extends RuntimeException {
     public UserFoundException() {
         super("User exists already");
--- src/main/java/patika/dev/definex/loancreditscore/config/exception/UserNotFoundException.java
+++ src/main/java/patika/dev/definex/loancreditscore/config/exception/UserNotFoundException.java
@@ -1,5 +1,8 @@
 package patika.dev.definex.loancreditscore.config.exception;
 
+/**
+ * This class is a RuntimeException that is thrown when a user is not found.
+ */
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super("User with ID: " + message + " not found");
