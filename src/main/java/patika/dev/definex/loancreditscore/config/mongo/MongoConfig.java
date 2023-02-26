3abd4821457843aba8cd0d0c00dd5343fc557fb8
aminet2a <aminetbo@gmail.com>
2023-02-26 18:12:54 +0100

    add comments to MongoConfig

--- src/main/java/patika/dev/definex/loancreditscore/config/mongo/MongoConfig.java
+++ src/main/java/patika/dev/definex/loancreditscore/config/mongo/MongoConfig.java
@@ -13,6 +13,13 @@ import java.util.Optional;
 @EnableMongoRepositories("patika.dev.definex.loancreditscore.repository")
 @EnableMongoAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 public class MongoConfig {
+    /**
+     * This method returns the current date and time as a LocalDateTime object.
+     * used for the @LastModifiedDate and @CreatedDate annotations used in the
+     * BaseModel, to keep track of the records insertion and updates
+     *
+     * @return Optional.of(LocalDateTime.now ())
+     */
     @Bean(name = "auditingDateTimeProvider")
     public DateTimeProvider dateTimeProvider() {
         return () -> Optional.of(LocalDateTime.now());
