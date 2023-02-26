a1d9b7840abed4cd5705898baa2ef1ba6d1e66a3
aminet2a <aminetbo@gmail.com>
2023-02-26 19:46:48 +0100

    add comments to IncomeRangeServiceImpl

--- src/main/java/patika/dev/definex/loancreditscore/service/common/IncomeRangeServiceImpl.java
+++ src/main/java/patika/dev/definex/loancreditscore/service/common/IncomeRangeServiceImpl.java
@@ -7,6 +7,13 @@ import patika.dev.definex.loancreditscore.enums.IncomeCategory;
 
 @Service
 public class IncomeRangeServiceImpl implements IncomeRangeService {
+    /**
+     * Method that return the client's income category depending on
+     * which range his/her provided monthly income belongs
+     *
+     * @param income
+     * @return Double
+     */
     @Override
     public IncomeCategory getIncomeCategory(Double income) {
         if (IncomeRange.LOW.contains(income))
