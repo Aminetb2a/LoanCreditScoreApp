1c452183ac64a4fef65e70e43156c6a77fb6ebce
aminet2a <aminetbo@gmail.com>
2023-02-26 20:06:25 +0100

    comment SmsGenerator

--- src/main/java/patika/dev/definex/loancreditscore/service/user/util/SmsGenerator.java
+++ src/main/java/patika/dev/definex/loancreditscore/service/user/util/SmsGenerator.java
@@ -5,6 +5,13 @@ import patika.dev.definex.loancreditscore.dto.user.UserDTO;
 
 @Component
 public class SmsGenerator {
+    /**
+     * Method to handle the content of the sms to be send to loan application users
+     *
+     * @param user usr
+     * @return Sms content
+     */
+
     public String generateSms(UserDTO user) {
         return String.format("Thank you for applying to our loan Mr(s) %s.\n\nThe result of you application is %s with a limit of %s.\nPlease contact our customer services for more details.\n\nThank you!",
                 user.getSurname(), user.getLoanStatus().toString().toLowerCase(), user.getCreditLimit());
