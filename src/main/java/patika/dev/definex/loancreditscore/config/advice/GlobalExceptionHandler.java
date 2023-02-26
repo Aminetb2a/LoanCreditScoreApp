5346227df75ef2c152c2e91ca989b57f60632f29
aminet2a <aminetbo@gmail.com>
2023-02-26 18:04:20 +0100

    add comments to GlobalExceptionHandler

--- src/main/java/patika/dev/definex/loancreditscore/config/advice/GlobalExceptionHandler.java
+++ src/main/java/patika/dev/definex/loancreditscore/config/advice/GlobalExceptionHandler.java
@@ -13,30 +13,65 @@ import patika.dev.definex.loancreditscore.dto.response.BaseResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * If a DuplicateKeyException is thrown, return a response with the status code 400 and the message
+     * "A user with this information is already registered."
+     *
+     * @param ex The exception object
+     * @return A BaseResponse object with a message.
+     */
     @ExceptionHandler({DuplicateKeyException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody BaseResponse duplicateKeyExceptionHandler(DuplicateKeyException ex) {
         return getResponse("A user with this information is already registered.");
     }
 
+    /**
+     * If an IllegalArgumentException is thrown, return a 400 Bad Request response with the exception
+     * message
+     *
+     * @param ex The exception object
+     * @return A BaseResponse object with the message from the exception.
+     */
     @ExceptionHandler({IllegalArgumentException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody BaseResponse illegalStateExceptionHandler(IllegalArgumentException ex) {
         return getResponse(ex.getMessage());
     }
 
+    /**
+     * If a UserNotFoundExceptionHandler is thrown, return a 400 Bad Request response with the exception
+     * message
+     *
+     * @param ex The exception object
+     * @return A BaseResponse object with the message from the exception.
+     */
     @ExceptionHandler({UserNotFoundException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public @ResponseBody BaseResponse UserNotFoundExceptionHandler(UserNotFoundException ex) {
+    public @ResponseBody BaseResponse userNotFoundExceptionHandler(UserNotFoundException ex) {
         return getResponse(ex.getMessage());
     }
 
+    /**
+     * If a UserFoundExceptionHandler is thrown, return a 400 Bad Request response with the exception
+     * message
+     *
+     * @param ex The exception object
+     * @return A BaseResponse object with the message from the exception.
+     */
     @ExceptionHandler({UserFoundException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public @ResponseBody BaseResponse UserFoundExceptionHandler(UserFoundException ex) {
+    public @ResponseBody BaseResponse userFoundExceptionHandler(UserFoundException ex) {
         return getResponse(ex.getMessage());
     }
 
+    /**
+     * Method returns a BaseResponse with the exception message.
+     *
+     * @param message The message that will be displayed to the user.
+     * @return A BaseResponse object with a with the exception message
+     * and a false success
+     */
     private BaseResponse getResponse(String message) {
         return BaseResponse.builder()
                 .message(message)
