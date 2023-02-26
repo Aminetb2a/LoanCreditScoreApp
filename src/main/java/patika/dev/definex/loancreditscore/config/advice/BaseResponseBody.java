7d71e4315a8c50e43a0610d6f2152c0662b23047
aminet2a <aminetbo@gmail.com>
2023-02-26 17:53:59 +0100

    add comments to BaseResponseBody

--- src/main/java/patika/dev/definex/loancreditscore/config/advice/BaseResponseBody.java
+++ src/main/java/patika/dev/definex/loancreditscore/config/advice/BaseResponseBody.java
@@ -12,16 +12,37 @@ import patika.dev.definex.loancreditscore.dto.response.BaseResponse;
 @RestControllerAdvice
 public class BaseResponseBody implements ResponseBodyAdvice {
 
+    /**
+     * If the class name contains the word "Exception" or "error" or "springdoc", then return false
+     * to skip the beforeBodyWrite method
+     *
+     * @param returnType    The return type of the method that is being called.
+     * @param converterType The converter type selected to use. Here, we're ensuring that the converter
+     *                      type is not of type ResponseBodyAdvice.
+     * @return A boolean value.
+     */
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
         String className = returnType.getContainingClass().toString();
         return !className.contains("Exception") && !className.contains("error") && !className.contains("springdoc");
     }
 
+    /**
+     * Mehtod that takes the response body, wraps it in a BaseResponse object, and returns it
+     *
+     * @param result                The data that will be returned to the client.
+     * @param returnType            The return type of the method that is being invoked.
+     * @param selectedContentType   The content type selected through content negotiation.
+     * @param selectedConverterType The converter type selected to write to the response body. This
+     *                              converter type will be processed unless the method returns void.
+     * @param request               The request object
+     * @param sResponse             The response object that will be returned to the client.
+     * @return A BaseResponse object
+     */
     @Override
-    public BaseResponse beforeBodyWrite(Object data, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse sResponse) {
+    public BaseResponse beforeBodyWrite(Object result, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse sResponse) {
         BaseResponse response = BaseResponse.builder()
-                .result(data)
+                .result(result)
                 .success(true)
                 .build();
         if (HttpMethod.DELETE.equals(request.getMethod()))
