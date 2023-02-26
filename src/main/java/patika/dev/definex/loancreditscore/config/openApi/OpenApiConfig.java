f218f352cbc1ba7eff53706d2fffb01852a2f9a0
aminet2a <aminetbo@gmail.com>
2023-02-26 18:19:38 +0100

    update OpenApiConfig add comments

--- src/main/java/patika/dev/definex/loancreditscore/config/openApi/OpenApiConfig.java
+++ src/main/java/patika/dev/definex/loancreditscore/config/openApi/OpenApiConfig.java
@@ -1,22 +1,30 @@
 package patika.dev.definex.loancreditscore.config.openApi;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Loan Credit Score API API", version = "0.1",
-        contact = @Contact(name = "aminetb2a", email = "aminetbo@gmail.com"))
-)
 public class OpenApiConfig {
-
-//    @Bean
-//    public OpenAPI springOpenAPI() {
-//        return new OpenAPI().info(new Info() //
-//                .title("Loan Credit Score API") //
-//                .description("Loan Credit Score API Simple Application") //
-//                .version("0.0.1"));
-//    }
+    /**
+     * This method shows the application's information, description,
+     * version and the owner's contact details.
+     *
+     * @return OpenAPI
+     */
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("Loan Credit Score API")
+                .description("Loan Credit Score API Simple Application")
+                .contact(new Contact()
+                        .name("Ahmed Amine T.B")
+                        .email("aminetbo@gmail.com")
+                        .url("https://aminetb2a.github.io/"))
+                .version("0.0.1"));
+    }
 
 }
